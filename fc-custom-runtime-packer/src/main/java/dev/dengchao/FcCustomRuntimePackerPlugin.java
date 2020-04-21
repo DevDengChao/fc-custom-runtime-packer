adb202b9be0bbed6a6f544a546a4dceda32f13f5
dengchao <2325690622@qq.com>
2020-04-21 20:49:32 +0800

    Optimize task name

--- fc-custom-runtime-packer/src/main/java/dev/dengchao/FcCustomRuntimePackerPlugin.java
+++ fc-custom-runtime-packer/src/main/java/dev/dengchao/FcCustomRuntimePackerPlugin.java
@@ -3,6 +3,7 @@ package dev.dengchao;
 import dev.dengchao.bootstrap.collector.ProjectBootstrapCollector;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.tasks.TaskContainer;
 import org.jetbrains.annotations.NotNull;
 
@@ -21,7 +22,13 @@ public class FcCustomRuntimePackerPlugin implements Plugin<Project> {
 
         for (Map.Entry<String, File> entry : entries) {
             String profile = entry.getKey();
-            String name = "zipBootstrap" + profile;
+
+            char c = profile.charAt(0);
+            if (c > 'a' && c < 'z') {
+                c += 'A' - 'a';
+            }
+
+            String name = "zipBootstrap" + c + profile.substring(1);
             tasks.create(name, ZipBootstrap.class, it -> {
                 it.setProfile(profile);
                 it.setBootstrap(entry.getValue());
